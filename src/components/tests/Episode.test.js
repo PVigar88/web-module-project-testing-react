022985ddee2de57836890f8206633be30a32c6ea
PVigar88 <Pvigar@gmail.com>
2021-06-02 22:33:19 -0400

    episode tests completed

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,32 +1,47 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Episode from './../Episode';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Episode from "./../Episode";
 
 const testEpisode = {
-    id:1,
-    name: "",
-    image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
-    season: 1,
-    number: 1,
-    summary: "",
-    runtime: 1
-}
+  id: 1,
+  name: "",
+  image:
+    "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+  season: 1,
+  number: 1,
+  summary: "This is a summary of the episode",
+  runtime: 1,
+};
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
-}
+  //Add in approprate test data structure here.
+  id: 1,
+  name: "",
+  season: 1,
+  number: 1,
+  summary: "This is a summary of the episode",
+  runtime: 1,
+};
 
 test("renders without error", () => {
-
+  render(<Episode episode={testEpisode} />);
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summury test passed as prop", () => {
+  render(<Episode episode={testEpisode} />);
+  const summary = screen.queryByText(/This is a summary of the episode/i);
+
+  expect(summary).not.toBeNull();
+  expect(summary).toBeInTheDocument();
 });
 
-test("renders default image when image is not defined", ()=>{
-    
-})
+test("renders default image when image is not defined", () => {
+  render(<Episode episode={testEpisodeWithoutImage} />);
+
+  const defaultImage = screen.queryByAltText("./stranger_things.png");
+  expect(defaultImage).not.toBeNull();
+  expect(defaultImage).toBeInTheDocument();
+});
 
 //Tasks
 //1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
